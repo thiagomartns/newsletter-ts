d6c0a2196b355ef9286b3accaae56086a8ce75ad
thiago <tmg3.mn@gmail.com>
2023-03-24 09:15:01 -0300

    Add 'MainGrid' component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import Header from './components/header/Header';
 import MainForm from './components/mainForm/MainForm';
+import MainGrid from './components/mainGrid/MainGrid';
 
 function App() {
 
@@ -9,126 +10,7 @@ function App() {
       <Header />
       <main>
         <MainForm />
-        <div className="main-grid">
-          <h1 className="main-title">Sua seleção especial</h1>
-          <div className="all-products-container">
-            <div className="product-container">
-              <img src="*" />
-              <div className="product-container--info">
-                <h2 className="product-title">Nome do Produto</h2>
-                <p className="product-description">
-                  Descrição do produto um pouco maior, com duas linhas ou três
-                  que explica melhor do que se trata.
-                </p>
-                <p className="product-old-price misc">De: R$23,99</p>
-                <h2 className="product-new-price">Por: R$19,99</h2>
-                <p className="misc">ou 2x de R$9,99</p>
-                <button className="buy-btn">Comprar</button>
-              </div>
-            </div>
-            <div className="product-container">
-              <img src="" />
-              <div className="product-container--info">
-                <h2 className="product-title">Nome do Produto</h2>
-                <p className="product-description">
-                  Descrição do produto um pouco maior, com duas linhas ou três
-                  que explica melhor do que se trata.
-                </p>
-                <p className="product-old-price misc">De: R$23,99</p>
-                <h2 className="product-new-price">Por: R$19,99</h2>
-                <p className="misc">ou 2x de R$9,99</p>
-                <button className="buy-btn">Comprar</button>
-              </div>
-            </div>
-            <div className="product-container">
-              <img src="" />
-              <div className="product-container--info">
-                <h2 className="product-title">Nome do Produto</h2>
-                <p className="product-description">
-                  Descrição do produto um pouco maior, com duas linhas ou três
-                  que explica melhor do que se trata.
-                </p>
-                <p className="product-old-price misc">De: R$23,99</p>
-                <h2 className="product-new-price">Por: R$19,99</h2>
-                <p className="misc">ou 2x de R$9,99</p>
-                <button className="buy-btn">Comprar</button>
-              </div>
-            </div>
-            <div className="product-container">
-              <img src="" />
-              <div className="product-container--info">
-                <h2 className="product-title">Nome do Produto</h2>
-                <p className="product-description">
-                  Descrição do produto um pouco maior, com duas linhas ou três
-                  que explica melhor do que se trata.
-                </p>
-                <p className="product-old-price misc">De: R$23,99</p>
-                <h2 className="product-new-price">Por: R$19,99</h2>
-                <p className="misc">ou 2x de R$9,99</p>
-                <button className="buy-btn">Comprar</button>
-              </div>
-            </div>
-            <div className="product-container">
-              <img src="" />
-              <div className="product-container--info">
-                <h2 className="product-title">Nome do Produto</h2>
-                <p className="product-description">
-                  Descrição do produto um pouco maior, com duas linhas ou três
-                  que explica melhor do que se trata.
-                </p>
-                <p className="product-old-price misc">De: R$23,99</p>
-                <h2 className="product-new-price">Por: R$19,99</h2>
-                <p className="misc">ou 2x de R$9,99</p>
-                <button className="buy-btn">Comprar</button>
-              </div>
-            </div>
-            <div className="product-container">
-              <img src="" />
-              <div className="product-container--info">
-                <h2 className="product-title">Nome do Produto</h2>
-                <p className="product-description">
-                  Descrição do produto um pouco maior, com duas linhas ou três
-                  que explica melhor do que se trata.
-                </p>
-                <p className="product-old-price misc">De: R$23,99</p>
-                <h2 className="product-new-price">Por: R$19,99</h2>
-                <p className="misc">ou 2x de R$9,99</p>
-                <button className="buy-btn">Comprar</button>
-              </div>
-            </div>
-            <div className="product-container">
-              <img src="" />
-              <div className="product-container--info">
-                <h2 className="product-title">Nome do Produto</h2>
-                <p className="product-description">
-                  Descrição do produto um pouco maior, com duas linhas ou três
-                  que explica melhor do que se trata.
-                </p>
-                <p className="product-old-price misc">De: R$23,99</p>
-                <h2 className="product-new-price">Por: R$19,99</h2>
-                <p className="misc">ou 2x de R$9,99</p>
-                <button className="buy-btn">Comprar</button>
-              </div>
-            </div>
-            <div className="product-container">
-              <img src="" />
-              <div className="product-container--info">
-                <h2 className="product-title">Nome do Produto</h2>
-                <p className="product-description">
-                  Descrição do produto um pouco maior, com duas linhas ou três
-                  que explica melhor do que se trata.
-                </p>
-                <p className="product-old-price misc">De: R$23,99</p>
-                <h2 className="product-new-price">Por: R$19,99</h2>
-                <p className="misc">ou 2x de R$9,99</p>
-                <button className="buy-btn">Comprar</button>
-              </div>
-            </div>
-          </div>
-          <button className="more-products--btn buy-btn">
-            Ainda mais produtos aqui!
-          </button>
-        </div>
+        <MainGrid />
         <div className="call-to-action">
           <h1 className="main-title">Compartilhe a Novidade</h1>
           <p>
---
+++ src/components/mainGrid/MainGrid.tsx
@@ -0,0 +1,129 @@
+import React from 'react'
+import './mainGrid.scss';
+
+const MainGrid = () => {
+  return (
+    <div className="main-grid">
+      <h1 className="main-title">Sua seleção especial</h1>
+      <div className="all-products-container">
+        <div className="product-container">
+          <img src="*" />
+          <div className="product-container--info">
+            <h2 className="product-title">Nome do Produto</h2>
+            <p className="product-description">
+              Descrição do produto um pouco maior, com duas linhas ou três
+              que explica melhor do que se trata.
+            </p>
+            <p className="product-old-price misc">De: R$23,99</p>
+            <h2 className="product-new-price">Por: R$19,99</h2>
+            <p className="misc">ou 2x de R$9,99</p>
+            <button className="buy-btn">Comprar</button>
+          </div>
+        </div>
+        <div className="product-container">
+          <img src="" />
+          <div className="product-container--info">
+            <h2 className="product-title">Nome do Produto</h2>
+            <p className="product-description">
+              Descrição do produto um pouco maior, com duas linhas ou três
+              que explica melhor do que se trata.
+            </p>
+            <p className="product-old-price misc">De: R$23,99</p>
+            <h2 className="product-new-price">Por: R$19,99</h2>
+            <p className="misc">ou 2x de R$9,99</p>
+            <button className="buy-btn">Comprar</button>
+          </div>
+        </div>
+        <div className="product-container">
+          <img src="" />
+          <div className="product-container--info">
+            <h2 className="product-title">Nome do Produto</h2>
+            <p className="product-description">
+              Descrição do produto um pouco maior, com duas linhas ou três
+              que explica melhor do que se trata.
+            </p>
+            <p className="product-old-price misc">De: R$23,99</p>
+            <h2 className="product-new-price">Por: R$19,99</h2>
+            <p className="misc">ou 2x de R$9,99</p>
+            <button className="buy-btn">Comprar</button>
+          </div>
+        </div>
+        <div className="product-container">
+          <img src="" />
+          <div className="product-container--info">
+            <h2 className="product-title">Nome do Produto</h2>
+            <p className="product-description">
+              Descrição do produto um pouco maior, com duas linhas ou três
+              que explica melhor do que se trata.
+            </p>
+            <p className="product-old-price misc">De: R$23,99</p>
+            <h2 className="product-new-price">Por: R$19,99</h2>
+            <p className="misc">ou 2x de R$9,99</p>
+            <button className="buy-btn">Comprar</button>
+          </div>
+        </div>
+        <div className="product-container">
+          <img src="" />
+          <div className="product-container--info">
+            <h2 className="product-title">Nome do Produto</h2>
+            <p className="product-description">
+              Descrição do produto um pouco maior, com duas linhas ou três
+              que explica melhor do que se trata.
+            </p>
+            <p className="product-old-price misc">De: R$23,99</p>
+            <h2 className="product-new-price">Por: R$19,99</h2>
+            <p className="misc">ou 2x de R$9,99</p>
+            <button className="buy-btn">Comprar</button>
+          </div>
+        </div>
+        <div className="product-container">
+          <img src="" />
+          <div className="product-container--info">
+            <h2 className="product-title">Nome do Produto</h2>
+            <p className="product-description">
+              Descrição do produto um pouco maior, com duas linhas ou três
+              que explica melhor do que se trata.
+            </p>
+            <p className="product-old-price misc">De: R$23,99</p>
+            <h2 className="product-new-price">Por: R$19,99</h2>
+            <p className="misc">ou 2x de R$9,99</p>
+            <button className="buy-btn">Comprar</button>
+          </div>
+        </div>
+        <div className="product-container">
+          <img src="" />
+          <div className="product-container--info">
+            <h2 className="product-title">Nome do Produto</h2>
+            <p className="product-description">
+              Descrição do produto um pouco maior, com duas linhas ou três
+              que explica melhor do que se trata.
+            </p>
+            <p className="product-old-price misc">De: R$23,99</p>
+            <h2 className="product-new-price">Por: R$19,99</h2>
+            <p className="misc">ou 2x de R$9,99</p>
+            <button className="buy-btn">Comprar</button>
+          </div>
+        </div>
+        <div className="product-container">
+          <img src="" />
+          <div className="product-container--info">
+            <h2 className="product-title">Nome do Produto</h2>
+            <p className="product-description">
+              Descrição do produto um pouco maior, com duas linhas ou três
+              que explica melhor do que se trata.
+            </p>
+            <p className="product-old-price misc">De: R$23,99</p>
+            <h2 className="product-new-price">Por: R$19,99</h2>
+            <p className="misc">ou 2x de R$9,99</p>
+            <button className="buy-btn">Comprar</button>
+          </div>
+        </div>
+      </div>
+      <button className="more-products--btn buy-btn">
+        Ainda mais produtos aqui!
+      </button>
+    </div>
+  )
+}
+
+export default MainGrid
